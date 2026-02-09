051389dad6cb24a308fb2222155f06c98207f700
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-02-09 17:59:39 +0100

    feat(ui): implement usePersonalSettings hook in Navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,17 +1,22 @@
 import Logo from "./Logo";
 import { classes } from "../../constants/layout/navbar";
 import { NavLink } from "react-router-dom";
+import { usePersonalSettings } from "../../hooks/usePersonalSettings";
 
-export default function Navbar () {
+export default function Navbar() {
+  const { container, btn, logo } = classes;
 
-    const {container, btn, logo} = classes
-    return (
-        <>
-        
-        <nav className={container}>
-            <NavLink to="/" className={logo} end><Logo /></NavLink>
-            <NavLink to="/login" className={btn}>login</NavLink>
-        </nav>
-        </>
-    )
+  usePersonalSettings();
+  return (
+    <>
+      <nav className={container}>
+        <NavLink to="/" className={logo} end>
+          <Logo />
+        </NavLink>
+        <NavLink to="/login" className={btn}>
+          login
+        </NavLink>
+      </nav>
+    </>
+  );
 }
