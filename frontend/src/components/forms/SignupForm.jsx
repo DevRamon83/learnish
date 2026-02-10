623fc1698a3a51d8c99435d82b1adf0852bb4f30
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-02-10 21:40:03 +0100

    feat: implement SignupForm using the new form factory logic

---
+++ frontend/src/components/forms/SignupForm.jsx
@@ -0,0 +1,32 @@
+import { signupConfig } from "../../configs/inputs/auth";
+import InputField from "../inputs/InputField";
+import { useFormFactory } from "../../hooks/factories/useFormFactory";
+
+const factoryConfig = {
+  targetKeys: ["username", "email", "psw", "confirmPassword"],
+  originalObjects: [
+    signupConfig.username,
+    signupConfig.email,
+    signupConfig.psw,
+    signupConfig.confirmPassword,
+  ],
+  addThisKeys: ["label", "placeholder"],
+  stringsAddress: ["components", "auth"],
+};
+
+export default function SignupForm() {
+  const formConfig = useFormFactory(factoryConfig);
+
+  return (
+    <>
+      {formConfig && (
+        <form>
+          <InputField dataField={formConfig.username} />
+          <InputField dataField={formConfig.email} />
+          <InputField dataField={formConfig.password} />
+          <InputField dataField={formConfig.passwordCheck} />
+        </form>
+      )}
+    </>
+  );
+}
