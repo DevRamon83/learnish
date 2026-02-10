7ad3e3a9bdf7dd13090ee11ca6184037f5d6dfb7
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-02-10 10:05:24 +0100

    feat: add signup route and update imports after renaming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import './App.css'
-import './styles/Main.scss'
-import Navbar from './components/layout/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
+import "./App.css";
+import "./styles/Main.scss";
+import Navbar from "./components/layout/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
 
 function App() {
-  
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
