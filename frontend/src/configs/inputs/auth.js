0310abf13d7e8b192de4fd6eb414f4a05122bb2c
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-02-10 21:42:54 +0100

    feat: define base input schemas for authentication fields

---
+++ frontend/src/configs/inputs/auth.js
@@ -0,0 +1,26 @@
+export const signupConfig = {
+  username: {
+    id: "username",
+    type: "text",
+    name: "username",
+    autoComplete: "username",
+  },
+  email: {
+    id: "email",
+    type: "email",
+    name: "email",
+    autoComplete: "email",
+  },
+  psw: {
+    id: "password",
+    type: "password",
+    name: "password",
+    autoComplete: "new-password",
+  },
+  confirmPassword: {
+    id: "passwordCheck",
+    type: "password",
+    name: "passwordCheck",
+    autoComplete: "new-password",
+  },
+};
