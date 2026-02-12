6b91cd95e11b8d9f10bcb6b0f65f5111d9346afc
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-02-12 16:16:06 +0100

    refactor: remove redundant name property from signupConfig

--- frontend/src/configs/inputs/auth.js
+++ frontend/src/configs/inputs/auth.js
@@ -2,25 +2,21 @@ export const signupConfig = {
   username: {
     id: "username",
     type: "text",
-    name: "username",
     autoComplete: "username",
   },
   email: {
     id: "email",
     type: "email",
-    name: "email",
     autoComplete: "email",
   },
-  psw: {
+  password: {
     id: "password",
     type: "password",
-    name: "password",
     autoComplete: "new-password",
   },
   confirmPassword: {
-    id: "passwordCheck",
+    id: "confirmPassword",
     type: "password",
-    name: "passwordCheck",
     autoComplete: "new-password",
   },
 };
