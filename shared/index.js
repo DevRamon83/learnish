a717bfda5171ce18ae375decccec50c1c810ebf7
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-27 17:13:05 +0100

    refactor(shared): modularize validation logic and update bundle

--- shared/index.js
+++ shared/index.js
@@ -1,12 +1,14 @@
 import constants from "./constants/index.js";
 import langs from "./languages/index.js";
 import { acceptOnly, usernameValidator } from "ramon-vanilla";
+import validators from "./validators/index.js";
 
 const bundle = {
   langs,
   constants,
   acceptOnly,
   usernameValidator,
+  validators,
 };
 
 export default bundle;
---
+++ shared/validators/index.js
@@ -0,0 +1,5 @@
+import authValidator from "./authValidator.js";
+
+const validators = { authValidator };
+
+export default validators;
