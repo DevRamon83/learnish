1648fa6ec4688b741b76c66a049dd0c0a3dca6f6
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-27 16:16:21 +0100

    refactor: centralize auth validation logic

---
+++ shared/validators/authValidator.js
@@ -0,0 +1,39 @@
+import {
+  emailValidator,
+  passwordValidator,
+  usernameValidator,
+} from "ramon-vanilla";
+import constants from "../constants";
+
+const authValidator = (caller, data) => {
+  const { username, email, confirmEmail } = data;
+  const validUser = usernameValidator(username);
+  if (validUser.error) return validUser.errorArray;
+
+  const isLogin = caller === "/login";
+
+  const validEmail = !isLogin && emailValidator(email);
+
+  if (validEmail.error) return validEmail.errorArray;
+
+  const matchFail = "match failed";
+
+  if (!isLogin && email !== confirmEmail) return "email " + matchFail;
+
+  const { password, confirmPassword, privacy, tos } = data;
+
+  const validPassword = passwordValidator(password);
+
+  if (validPassword.error) return validPassword.errorArray;
+
+  if (!isLogin && password !== confirmPassword) return "password " + matchFail;
+
+  if (!isLogin && privacy !== constants.currentPrivacy)
+    return "invalid privacy policy";
+
+  if (!isLogin && tos !== constants.currentTos) return "invalid tos";
+
+  return false;
+};
+
+export default authValidator;
