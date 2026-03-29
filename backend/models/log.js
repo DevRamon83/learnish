5535384f7b481b52bdae9266c1006b7876aa4a51
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-29 14:39:00 +0200

    feat: add security log schema

---
+++ backend/models/log.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const LogSchema = new Schema(
+  {
+    username: { type: String },
+    ip: { type: String },
+    method: { type: String },
+    url: { type: String },
+    errorType: { type: String },
+    userAgent: { type: String },
+  },
+  { timestamps: true },
+);
+
+const logModel = mongoose.model("log", LogSchema);
+
+export default logModel;
