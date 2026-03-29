2be8dcbb7e7a70b65fc13a8540d563658bc95920
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-29 14:45:06 +0200

    feat: integrate security logging in error handler

--- backend/helpers/handleErrorResponse.js
+++ backend/helpers/handleErrorResponse.js
@@ -1,10 +1,28 @@
-const handleErrorResponse = (res, req, message, status, log, ipBann) => {
+import bannUser from "../services/bannUser.js";
+import logWriter from "../services/logWriter.js";
+
+const logData = (req, message, userBann) => {
+  return {
+    username: userBann ? userBann : null,
+    method: req.method,
+    ip: req.ip,
+    url: req.originalUrl,
+    errorType: message,
+    userAgent: req.headers["user-agent"],
+  };
+};
+
+const handleErrorResponse = (res, req, message, status, log, userBann) => {
   const ensureMessage = log ? "error_fakeMsg" : message;
   res.status(status).json({ error: true, message: ensureMessage });
   if (log) {
-    // write log logic
+    const data = logData(req, message, userBann);
+    logWriter(data);
   }
-  return true;
+
+  if (userBann) {
+    bannUser(userBann, req);
+  }
 };
 
 export default handleErrorResponse;
