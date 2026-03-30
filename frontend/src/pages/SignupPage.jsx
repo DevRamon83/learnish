3bb2dc3121d7d3cee51298e78384161ca1864bf7
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-30 16:00:13 +0200

    refactor: change wrapper div to main

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,9 +2,9 @@ import SignupForm from "../components/forms/SignupForm";
 
 export default function SignupPage() {
   return (
-    <>
+    <main>
       <div>registrati</div>
       <SignupForm />
-    </>
+    </main>
   );
 }
