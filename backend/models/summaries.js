0948c2581e6973a470a68c3046bb966e10d9b402
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-31 10:09:55 +0200

    refactor: add video details to summary schema

--- backend/models/summaries.js
+++ backend/models/summaries.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 
 const SummarySchema = new Schema(
   {
+    title: { type: String, required: true },
+    channel: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    url: { type: String, required: true },
     original: { type: String, required: true },
     ai: { type: String, required: true },
     owner: {
