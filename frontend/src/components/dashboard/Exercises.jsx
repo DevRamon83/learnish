ea7b94fe51c53d544b832865f2c540a4579f7aab
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-31 19:24:59 +0200

    feat: implement conditional rendering for Summaries

--- frontend/src/components/dashboard/Exercises.jsx
+++ frontend/src/components/dashboard/Exercises.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { classes, exercisesTab } from "../../constants/layout/dashboard";
 import ExercisesMenu from "./ExercisesMenu";
+import Summaries from "./summaries/Summaries";
 
 export default function Exercises() {
   const [exercise, setExercise] = useState(exercisesTab[0]);
   return (
-    <div className={classes.dashPanel}>
-      <ExercisesMenu exercise={exercise} setExercise={setExercise} />
-    </div>
+    <>
+      <div className={classes.dashPanel}>
+        <ExercisesMenu exercise={exercise} setExercise={setExercise} />
+      </div>
+      {exercise === "sommario" && <Summaries />}
+    </>
   );
 }
