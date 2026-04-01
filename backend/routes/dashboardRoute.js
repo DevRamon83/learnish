562b681952869c0a42d531b338324dfae6c182ef
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-01 16:22:10 +0200

    feat: add validation middleware to newSummary endpoint

--- backend/routes/dashboardRoute.js
+++ backend/routes/dashboardRoute.js
@@ -2,11 +2,17 @@ import express from "express";
 import protectedRouth from "../middlewares/protectedRouth.js";
 import newSummary from "../controllers/dashboard/newSummary.js";
 import summariesList from "../controllers/dashboard/summariesList.js";
+import validateSummary from "../middlewares/dashboard/validateSummary.js";
 
 const dashboardRoute = express.Router();
 
 // need specific validation middleware
-dashboardRoute.post("/newSummary", ...protectedRouth, newSummary);
-dashboardRoute.post("/summariesList", ...protectedRouth, summariesList);
+dashboardRoute.post(
+  "/newSummary",
+  validateSummary,
+  ...protectedRouth,
+  newSummary,
+);
+dashboardRoute.get("/summariesList", ...protectedRouth, summariesList);
 
 export default dashboardRoute;
