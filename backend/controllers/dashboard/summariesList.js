ba7e13d2dfcbf7cfee197bee641f8903b4db47d8
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-01 16:12:04 +0200

    feat: add controller to fetch all user summaries

--- backend/controllers/dashboard/summariesList.js
+++ backend/controllers/dashboard/summariesList.js
@@ -1,8 +1,16 @@
 import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import summaryModel from "../../models/summaries.js";
 
 const summariesList = async (req, res) => {
   const log = false;
   try {
+    const userId = req.context.auth.id;
+    if (userId) {
+      // logga
+    }
+
+    const userSummaries = await summaryModel.find({ owner: userId }).lean();
+    return res.status(200).json(userSummaries);
   } catch (err) {
     console.error("Error in login:", err);
     return handleErrorResponse(res, req, err.message, 500, log);
