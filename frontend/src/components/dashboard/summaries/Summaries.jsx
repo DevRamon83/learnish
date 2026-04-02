e4036ce49beb27750c624c6e0c52ce168a02d645
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-02 19:23:58 +0200

    feat: implement retry logic for fetch re-triggering

--- frontend/src/components/dashboard/summaries/Summaries.jsx
+++ frontend/src/components/dashboard/summaries/Summaries.jsx
@@ -2,29 +2,39 @@ import { useState } from "react";
 import fetchSummaries from "../../../api/handlers.js/fetchSummaries";
 import { useEffect } from "react";
 import SummaryCard from "../../../ui/SummaryCard";
+import FetchObserver from "../../../api/FetchObserver";
 
 export default function Summaries() {
   const [summaries, setSummaries] = useState(null);
   const [panel, setPanel] = useState(null);
   const [open, setOpen] = useState(false);
+  const [fetchStatus, setFetchStatus] = useState(null);
+  const [retry, setRetry] = useState(0);
 
   useEffect(() => {
     const controller = new AbortController();
 
     const loadData = async () => {
+      setFetchStatus(null);
+
       const data = await fetchSummaries(controller.signal);
       if (data.error) {
-        // handle error
+        setFetchStatus("fail");
         return;
       }
-      console.log("data ", data);
-      setSummaries(data);
+
+      if (data.length === 0) {
+        setFetchStatus("void");
+      } else {
+        setFetchStatus(null);
+        setSummaries(data);
+      }
     };
 
     loadData();
 
     return () => controller.abort();
-  }, []);
+  }, [retry]);
 
   return (
     <>
@@ -33,9 +43,13 @@ export default function Summaries() {
           <h1>i tuoi sommari</h1>
           <button onClick={() => setPanel(true)}>Nuovo sommario</button>
 
-          {!summaries || summaries.length === 0 ? (
-            <p>Nessun sommario</p>
-          ) : (
+          <FetchObserver
+            fetchStatus={fetchStatus}
+            caller={"sommario"}
+            setRetry={setRetry}
+          />
+
+          {summaries &&
             summaries.map((summary) => (
               <SummaryCard
                 key={summary._id}
@@ -43,8 +57,7 @@ export default function Summaries() {
                 open={open}
                 setter={setOpen}
               />
-            ))
-          )}
+            ))}
         </>
       )}
 
