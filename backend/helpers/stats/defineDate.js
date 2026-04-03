7684295c187fcda4c5e48f44b4253d0639c1629a
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-03 12:25:22 +0200

    feat: add defineDate helper to extract day, month, and year

---
+++ backend/helpers/stats/defineDate.js
@@ -0,0 +1,9 @@
+const defineDate = (myDate) => {
+  const date = new Date(myDate);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return { day, month, year };
+};
+
+export default defineDate;
