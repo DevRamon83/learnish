4fd21ceb9543589f832c581c601e3f8347196dec
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-04 10:14:47 +0200

    refactor(front): receiva lang and string as props

--- frontend/src/components/dashboard/summaries/NewSummary.jsx
+++ frontend/src/components/dashboard/summaries/NewSummary.jsx
@@ -1,6 +1,4 @@
 import { TextareaInput, UrlInput, useRamonForm } from "ramon-form-sdude";
-import { i18nAddresses } from "../../../constants/i18nAddresses";
-import { useLang } from "../../../hooks/useLang";
 import FormInput from "../../forms/FormInput";
 import { useState } from "react";
 import { useRef } from "react";
@@ -10,11 +8,11 @@ import fetchNewSummaries from "../../../api/handlers.js/fetchNewSummary";
 import bundle from "shared";
 const { summaryValidator } = bundle.validators;
 
-export default function NewSummary({ panel, setPanel }) {
+export default function NewSummary({ panel, setPanel, strings, lang }) {
   const [error, setError] = useState(null);
   const formRef = useRef();
-  const { strings, lang } = useLang(i18nAddresses.summary);
-  const objConfig = summaryConfigBuilder(strings, "login");
+
+  const objConfig = summaryConfigBuilder(strings);
   const { fields, textareas } = useRamonForm(objConfig);
   const { summary } = textareas;
 
@@ -59,12 +57,12 @@ export default function NewSummary({ panel, setPanel }) {
       {panel && (
         <>
           <button onClick={() => setPanel(false)}>chiudi</button>
-          <p>crea un nuovo sommario</p>
+          <p>{strings.newSummary}</p>
           <form ref={formRef} onSubmit={submitHandler}>
             <FormInput Element={UrlInput} data={fields.youtube} lang={lang} />
             <FormInput Element={TextareaInput} data={summary} lang={lang} />
             <ErrorOnSubmit error={error} />
-            <button>Invia</button>
+            <button>{strings.sendSummary}</button>
           </form>
         </>
       )}
