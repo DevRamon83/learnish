0aa44a5879d37b434a6af9d615ecc125acf64320
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-04 09:56:55 +0200

    feat: Add useLang hook to Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import TabMenu from "../components/dashboard/TabMenu";
 import Exercises from "../components/dashboard/Exercises";
-import { dashboardTab } from "../constants/layout/dashboard";
+import { useLang } from "../hooks/useLang";
+import { i18nAddresses } from "../constants/i18nAddresses";
 
 export default function Dashboard() {
+  const { strings, lang } = useLang(i18nAddresses.dashboard);
+  const dashboardTab = strings.dashboardTab;
+  const exercisesTab = strings.exercisesTab;
   const [tabMenu, setTabMenu] = useState(dashboardTab[0]);
   return (
     <main>
       <aside>
         <nav>
-          <TabMenu tabMenu={tabMenu} setTabMenu={setTabMenu} />
+          <TabMenu
+            dashboardTab={dashboardTab}
+            tabMenu={tabMenu}
+            setTabMenu={setTabMenu}
+          />
         </nav>
       </aside>
-      <div className="dashboard">{tabMenu === "esercizi" && <Exercises />}</div>
+      <div className="dashboard">
+        {tabMenu === "esercizi" && <Exercises exercisesTab={exercisesTab} />}
+      </div>
     </main>
   );
 }
