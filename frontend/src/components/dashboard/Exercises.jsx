439deda1164c1517f1680be555f1f35394a470ff
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-04 10:12:27 +0200

    refactor(front): pass exercisesTab as props

--- frontend/src/components/dashboard/Exercises.jsx
+++ frontend/src/components/dashboard/Exercises.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import { classes, exercisesTab } from "../../constants/layout/dashboard";
+import { classes } from "../../constants/layout/dashboard";
 import ExercisesMenu from "./ExercisesMenu";
 import Summaries from "./summaries/Summaries";
 
-export default function Exercises() {
+export default function Exercises({ exercisesTab }) {
   const [exercise, setExercise] = useState(exercisesTab[0]);
   return (
     <>
       <div className={classes.dashPanel}>
-        <ExercisesMenu exercise={exercise} setExercise={setExercise} />
+        <ExercisesMenu
+          exercisesTab={exercisesTab}
+          exercise={exercise}
+          setExercise={setExercise}
+        />
       </div>
       {exercise === "sommario" && <Summaries />}
     </>
