11e9d35645b632e5f69c84bf9c989812ae6587f3
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-06 18:17:02 +0200

    feat(front): add constants for Hamburger

--- frontend/src/constants/layout/navbar.js
+++ frontend/src/constants/layout/navbar.js
@@ -1,5 +1,10 @@
 export const classes = {
-    container: "navbar__container",
-    btn: "navbar__btn",
-    logo: "navbar__btn-logo"
-}
+  container: "navbar__container",
+  btn: "navbar__btn",
+  logo: "navbar__btn-logo",
+  navbarContainer: "navbar__container",
+  navbarContainerActive: "navbar__container-active",
+  hamburger: "navbar__hamburger",
+  hamburgerBar: "navbar__hamburgerBar",
+  hamburgerBarActive: "navbar__hamburger-active",
+};
