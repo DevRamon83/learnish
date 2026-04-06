662d5e7f15d1c47fd91771823a3db88c64079b6e
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-06 18:18:03 +0200

    feat(front): add useViewportObeserver

---
+++ frontend/src/hooks/useViewportObserver.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export default function useViewportObserver() {
+  const [chartWidth, setChartWhidt] = useState(0);
+  const [viewport, setViewport] = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    let timeoutId = null;
+
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setViewport(window.innerWidth);
+      }, 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (viewport <= 600) {
+      const percent = (viewport / 100) * 80;
+      setChartWhidt(percent);
+      setIsMobile(true);
+    } else {
+      setChartWhidt(600);
+      setIsMobile(false);
+    }
+  }, [viewport]);
+
+  return { chartWidth, isMobile };
+}
