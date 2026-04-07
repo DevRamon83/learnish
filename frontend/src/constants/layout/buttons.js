78cfcb0f0c122bdc6865a1e00cf7181d66a1202f
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-07 15:39:18 +0200

    feat(front): add buttons

---
+++ frontend/src/constants/layout/buttons.js
@@ -0,0 +1,16 @@
+export const classes = {
+  double: {
+    container: "btn__double-imgContainerLeft",
+    leftImgContainer: "btn__double-imgContainerLeft",
+    leftImg: "btn__double-imgLeft",
+    rightImgContainer: "btn__double-imgContainerRight",
+    rightImg: "btn__double-imgRight",
+    text: "btn__double-text",
+  },
+  iconBtn: {
+    container: "btn__icontBtn-container",
+    img: "btn__icontBtn-img",
+    text: "btn__iconBtn-text",
+  },
+  legal: "btn__simple-legal",
+};
