a159d1fa79a20ef68ac1502a4c0202ae83b1b186
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-09 17:37:42 +0200

    refactor(back): generate verifyUrl based on NODE_ENV in createUser

--- backend/controllers/auth/createUser.js
+++ backend/controllers/auth/createUser.js
@@ -21,8 +21,10 @@ const createUser = async (req, res) => {
     const password = data.password;
     const hash = await argon2.hash(password);
     const confirmationToken = uuidv4();
-    const verifyUrl = process.env.ORIGIN_DEV + "/verify/" + confirmationToken;
-    console.log("verifyUrl ", verifyUrl);
+    const env = process.env.NODE_ENV;
+    const ORIGIN =
+      env === "DEV" ? process.env.ORIGIN_DEV : process.env.ORIGIN_PROD;
+    const verifyUrl = ORIGIN + "/verify/" + confirmationToken;
 
     const userObj = {
       username: data.username.trim().toLowerCase(),
@@ -37,7 +39,7 @@ const createUser = async (req, res) => {
 
     await model.create(userObj);
 
-    return res.status(201).json("created");
+    return res.status(201).json({ url: verifyUrl, message: "created" });
   } catch (err) {
     console.error("Error in createUser:", err);
     const log = false;
