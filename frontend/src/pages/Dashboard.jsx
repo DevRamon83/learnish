0314d6fdaa274c4189897691805c53e5202f3704
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-09 14:25:25 +0200

    feat(front): add logoutHandler to Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TabMenu from "../components/dashboard/TabMenu";
 import Exercises from "../components/dashboard/Exercises";
 import { useLang } from "../hooks/useLang";
 import { i18nAddresses } from "../constants/i18nAddresses";
+import fetchLogout from "../api/handlers.js/fetchLogout";
+import { useDispatch } from "react-redux";
+import { setAuth, setUser } from "../redux/slices/authSlice";
 
 export default function Dashboard() {
   const { strings, lang } = useLang(i18nAddresses.dashboard);
-  const dashboardTab = strings.dashboardTab;
+  const dispatch = useDispatch();
   const exercisesTab = strings.exercisesTab;
-  const [tabMenu, setTabMenu] = useState(dashboardTab[0]);
+  const [tabMenu, setTabMenu] = useState("study");
+
+  const logoutHandler = async () => {
+    const response = await fetchLogout();
+    if (response.error) {
+      console.error(response.errorMsg);
+    } else {
+      dispatch(setUser(null));
+      dispatch(setAuth("unauthenticated"));
+    }
+  };
+
+  useEffect(() => {
+    if (tabMenu === "logout") {
+      logoutHandler();
+    }
+  }, [tabMenu]);
+
   return (
     <main>
-      <aside>
-        <nav>
-          <TabMenu
-            dashboardTab={dashboardTab}
-            tabMenu={tabMenu}
-            setTabMenu={setTabMenu}
-          />
-        </nav>
-      </aside>
+      <TabMenu setTabMenu={setTabMenu} tabMenu={tabMenu} />
       <div className="dashboard">
         {tabMenu === "esercizi" && <Exercises exercisesTab={exercisesTab} />}
       </div>
