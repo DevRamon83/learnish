5335d58e5a03ba4df08257ea2980abe4d49d0458
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-11 09:42:48 +0200

    refactor(front): extract classNames and IDs for logo

---
+++ frontend/src/constants/layout/logo.js
@@ -0,0 +1,16 @@
+export const ids = {
+  el: "logo__el",
+  er: "logo__er",
+  en: "logo__en",
+  e: "logo__e",
+  a: "logo__a",
+  squareOpen: "logo__squareOpen",
+  squareClose: "logo__squareClose",
+  i: "logo__i",
+  es: "logo__es",
+  aitch: "logo__aitch",
+};
+
+export const classes = {
+  logo: "navbar__logo",
+};
