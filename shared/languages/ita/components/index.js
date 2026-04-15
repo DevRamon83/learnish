6b09017ce096033648c32fab36a8439bb49e2261
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-15 11:28:16 +0200

    fix(shared): import in ita components

--- shared/languages/ita/components/index.js
+++ shared/languages/ita/components/index.js
@@ -1,7 +1,7 @@
 import auth from "./auth.json" with { type: "json" };
 import summary from "./summary.json" with { type: "json" };
 import dashboard from "./dashboard.json" with { type: "json" };
-import navbar from "./navbar.json" with { type: "json" };
+import layout from "./layout.json" with { type: "json" };
 import errors from "./errors.json" with { type: "json" };
 import faq from "./faq.json" with { type: "json" };
 import team from "./team.json" with { type: "json" };
@@ -11,7 +11,7 @@ const components = {
   auth,
   summary,
   dashboard,
-  navbar,
+  layout,
   errors,
   faq,
   team,
