c6abfe07beff18664e6dbc71e654f55a40eada2e
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-17 15:14:40 +0200

    feat(back): add tokensRotation middleware

---
+++ backend/middlewares/auth/tokensRotation.js
@@ -0,0 +1,31 @@
+import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import createTokenConfigs from "../../helpers/token/createTokenConfigs.js";
+import setTokenAndCookie from "../../helpers/token/setTokenAndCookie.js";
+
+const tokensRotation = async (req, res, next) => {
+  const needRotation = req.context.tokens.rotate;
+  if (!needRotation) {
+    next();
+    return;
+  }
+  const log = true;
+
+  const { username, id, shared } = req.context.tokens.payload;
+
+  if (!username || !id || !shared) {
+    const errorMsg = "invalidPayload";
+    return handleErrorResponse(res, req, errorMsg, 404, log);
+  }
+
+  const configData = { username, id, shared };
+
+  const { accessConfig, refreshConfig } = createTokenConfigs(configData);
+
+  await setTokenAndCookie(res, accessConfig);
+
+  await setTokenAndCookie(res, refreshConfig);
+
+  next();
+};
+
+export default tokensRotation;
