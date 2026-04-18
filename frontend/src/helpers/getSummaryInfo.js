622bb3444d19884ba472a4701bb07888f21f95c4
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-18 14:16:48 +0200

    refactor(front): remove obsolete getSummaryStatus from getSummaryInfo

--- frontend/src/helpers/getSummaryInfo.js
+++ frontend/src/helpers/getSummaryInfo.js
@@ -6,13 +6,6 @@ export const getSummaryTitle = (summaryTitle) => {
   }
 };
 
-export const getSummaryStatus = (summary) => {
-  const { isDraft, misalignment } = summary;
-  if (isDraft) return "correggi";
-  if (misalignment) return "genera statistiche";
-  return "apri";
-};
-
 export const getSummaryDate = (date) => {
   return new Date(date).toLocaleDateString(undefined, {
     day: "numeric",
