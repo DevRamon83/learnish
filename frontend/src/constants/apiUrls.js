e8e00e4ce2f77fba334182d3635f261aaf9f8589
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-19 15:39:37 +0200

    feat(front) add stats address to API_URLS

--- frontend/src/constants/apiUrls.js
+++ frontend/src/constants/apiUrls.js
@@ -14,6 +14,7 @@ export const API_URLS = {
     summaries: "/summariesList",
     summary: "/newSummary",
     correction: "/aiCorrection",
+    stats: "/newStat",
   },
   messages: {
     base: "/messages",
