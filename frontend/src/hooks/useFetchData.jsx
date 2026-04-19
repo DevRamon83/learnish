5f0d2d88307ac84ebce9f179b44ea09a16d1193e
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-19 15:48:37 +0200

    feat(front): add useFetchData
    - Abstract hook for reactive data fetching

---
+++ frontend/src/hooks/useFetchData.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const useFetchData = (fetchHandler) => {
+  const [fetchStatus, setFetchStatus] = useState(null);
+  const [retry, setRetry] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const loadData = async () => {
+      setFetchStatus(null);
+
+      const res = await fetchHandler(controller.signal);
+      if (res.error) {
+        setFetchStatus("fail");
+        return;
+      }
+
+      if (res.length === 0) {
+        setFetchStatus("void");
+      } else {
+        setFetchStatus(null);
+        setData(res);
+      }
+    };
+
+    loadData();
+
+    return () => controller.abort();
+  }, [retry]);
+
+  return { fetchStatus, setFetchStatus, retry, setRetry, data };
+};
+
+export default useFetchData;
