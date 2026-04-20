7e48c298aa9eb7d7234ee77262326fddff26d4e1
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-20 11:47:57 +0200

    feat(front): add ReadSummary

---
+++ frontend/src/components/dashboard/summaries/ReadSummary.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import SummaryAction from "../../../ui/SummaryAction";
+import { useLang } from "../../../hooks/useLang";
+import { i18nAddresses } from "../../../constants/i18nAddresses";
+import fetchCorrection from "../../../api/handlers/fetchCorrection";
+import SummaryYoutubeBtn from "../../../ui/SummaryYoutubeBtn";
+import SummaryOpenBtn from "../../../ui/SummaryOpenBtn";
+
+export default function ReadSummary({
+  setter,
+  summary,
+  setMySummary,
+  setCurrentError,
+  classes,
+}) {
+  const [status, setStatus] = useState(null);
+  const [scoreClass, setScoreClass] = useState(null);
+  const { lang, strings } = useLang(i18nAddresses.summary);
+  const { score } = classes.summary;
+  const draft = summary.isDraft;
+  const misalignment = summary.misalignment;
+  const alert = draft || misalignment;
+
+  const openAlert = () => {
+    if (draft) setStatus("draft");
+    if (misalignment) setStatus("misalignment");
+  };
+
+  const actionHandler = async (action) => {
+    let response = null;
+    if (action === "correction") {
+      const data = { id: summary._id, lang };
+      response = await fetchCorrection(data);
+    } else if (action === "stats") {
+      // api stat alignment
+    }
+
+    if (response.error) {
+    } else {
+      setMySummary(response.summary);
+      setCurrentError(response.summary.errorCodes[0]);
+      setStatus(null);
+    }
+  };
+
+  useEffect(() => {
+    if (!summary.score) return;
+    if (summary.score.overall <= 3) {
+      setScoreClass(score.bad);
+    } else if (summary.score.overall <= 6) {
+      setScoreClass(score.medium);
+    } else {
+      setScoreClass(score.good);
+    }
+  }, [summary]);
+
+  const url = `https://youtu.be/${summary.videoID}`;
+
+  return (
+    <div className={classes.summary.iconContainer}>
+      {alert && (
+        <div onClick={openAlert} className={classes.summary.iconAlert}>
+          <img className={classes.summary.alertImg} src="/alert.svg" />
+        </div>
+      )}
+      <SummaryAction
+        actionHandler={actionHandler}
+        closeAlert={setStatus}
+        status={status}
+      />
+      {!alert && (
+        <>
+          <SummaryOpenBtn id={summary._id} setter={setter} />
+          <SummaryYoutubeBtn url={url} />
+          <div className={scoreClass}>{summary.score?.overall}</div>
+        </>
+      )}
+    </div>
+  );
+}
