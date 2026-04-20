ebf132017b194c3443cfb46d013e67006e1b2acf
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-20 13:03:46 +0200

    feat(front): add ShowSummaryDetails to ShowSummary

--- frontend/src/ui/ShowSummary.jsx
+++ frontend/src/ui/ShowSummary.jsx
@@ -1,4 +1,5 @@
 import { classes } from "../constants/layout/dashboard";
+import ShowSummaryDetails from "./ShowSummaryDetails";
 import SummaryError from "./SummaryError";
 
 export default function ShowSummary({
@@ -9,24 +10,27 @@ export default function ShowSummary({
   tooltipHandler,
 }) {
   return (
-    <>
+    <div className="summary__read-panel">
       {parse.length !== 0 && !isInvalid && (
-        <p className={classes.container}>
-          {parse.map((node) =>
-            node.type === "span" ? (
-              <span key={node.id}>{node.text}</span>
-            ) : (
-              <SummaryError
-                key={node.id}
-                node={node}
-                data={data}
-                toggle={toggle}
-                tooltipHandler={tooltipHandler}
-              />
-            ),
-          )}
-        </p>
+        <div className="summary__text-container">
+          <p className={classes.container}>
+            {parse.map((node) =>
+              node.type === "span" ? (
+                <span key={node.id}>{node.text}</span>
+              ) : (
+                <SummaryError
+                  key={node.id}
+                  node={node}
+                  data={data}
+                  toggle={toggle}
+                  tooltipHandler={tooltipHandler}
+                />
+              ),
+            )}
+          </p>
+        </div>
       )}
-    </>
+      <ShowSummaryDetails data={data} />
+    </div>
   );
 }
