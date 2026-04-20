0e4caa5d359f758a3c44d3d0a1c2ab8badc742d8
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-20 12:24:44 +0200

    feat(front): add useFetchData to Summaries

--- frontend/src/components/dashboard/summaries/Summaries.jsx
+++ frontend/src/components/dashboard/summaries/Summaries.jsx
@@ -1,83 +1,50 @@
 import { useState } from "react";
 import fetchSummaries from "../../../api/handlers/fetchSummaries";
-import { useEffect } from "react";
 import SummaryCard from "./SummaryCard";
 import FetchObserver from "../../../api/FetchObserver";
 import NewSummary from "./NewSummary";
 import { useLang } from "../../../hooks/useLang";
 import { i18nAddresses } from "../../../constants/i18nAddresses";
 import { useHideOverflow } from "../../../hooks/useHideOverflow";
+import useFetchData from "../../../hooks/useFetchData";
+import { classes } from "../../../constants/layout/dashboard";
 
 export default function Summaries() {
-  const [summaries, setSummaries] = useState(null);
   const [panel, setPanel] = useState(null);
   const [open, setOpen] = useState(false);
-  const [fetchStatus, setFetchStatus] = useState(null);
-  const [retry, setRetry] = useState(0);
   const { strings, lang } = useLang(i18nAddresses.summary);
   useHideOverflow(open);
-  useEffect(() => {
-    const controller = new AbortController();
-
-    const loadData = async () => {
-      setFetchStatus(null);
-
-      const data = await fetchSummaries(controller.signal);
-      if (data.error) {
-        setFetchStatus("fail");
-        return;
-      }
-
-      if (data.length === 0) {
-        setFetchStatus("void");
-      } else {
-        setFetchStatus(null);
-        setSummaries(data);
-      }
-    };
-
-    loadData();
-
-    return () => controller.abort();
-  }, [retry]);
+  const { fetchStatus, setRetry, data } = useFetchData(fetchSummaries);
 
   return (
     <>
-      {!panel && (
-        <>
-          <h1>{strings.yourSummaries}</h1>
-          <button onClick={() => setPanel(true)}>
-            {strings.newSummaryBtn}
-          </button>
+      <h1>{strings.yourSummaries}</h1>
+      <button onClick={() => setPanel(true)}>{strings.newSummaryBtn}</button>
 
-          <FetchObserver
-            fetchStatus={fetchStatus}
-            caller={"sommario"}
-            setRetry={setRetry}
-          />
+      <FetchObserver
+        fetchStatus={fetchStatus}
+        caller={strings.summary}
+        setRetry={setRetry}
+      />
 
-          <div className="dashboard__summaries">
-            {summaries &&
-              summaries.map((summary) => (
-                <SummaryCard
-                  key={summary._id}
-                  summary={summary}
-                  open={open}
-                  setter={setOpen}
-                />
-              ))}
-          </div>
-        </>
-      )}
+      <div className={classes.summary.list}>
+        {data &&
+          data.map((summary) => (
+            <SummaryCard
+              key={summary._id}
+              summary={summary}
+              open={open}
+              setter={setOpen}
+            />
+          ))}
+      </div>
 
-      {panel && (
-        <NewSummary
-          panel={panel}
-          setPanel={setPanel}
-          strings={strings}
-          lang={lang}
-        />
-      )}
+      <NewSummary
+        panel={panel}
+        setPanel={setPanel}
+        strings={strings}
+        lang={lang}
+      />
     </>
   );
 }
