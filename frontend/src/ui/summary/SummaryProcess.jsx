af86f8a6d972b4c465ab7405a3f5e119cc1826b0
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-21 15:13:17 +0200

    feat(front): add retry to SummaryProcess

--- frontend/src/ui/summary/SummaryProcess.jsx
+++ frontend/src/ui/summary/SummaryProcess.jsx
@@ -1,13 +1,42 @@
-export default function SummaryProcess({ step, status, label, bar }) {
+export default function SummaryProcess({
+  step,
+  status,
+  label,
+  bar,
+  setters,
+  strings,
+  value,
+}) {
+  const { setUploadStatus, setRetry, setUploadStep } = setters;
+
+  const retryHandler = () => {
+    setUploadStatus((prev) => ({ ...prev, [value]: null }));
+    const newStep = value === "draft" ? true : null;
+    setUploadStep((prev) => ({ ...prev, [value]: newStep }));
+    setRetry((prev) => prev + 1);
+  };
+
   return (
     <div className="summary__process-space">
-      <div className="summary__process-label">
-        {label}
-        <img src={`/${status}.svg`} />
-      </div>
-      <div className="summary__process-container">
-        <div className={step ? `${bar}-process` : bar}></div>
-      </div>
+      {status === "failed" ? (
+        <>
+          <div className="summary__process-label">
+            Qualcosa è andato storto
+            <img src={`/${status || "pending"}.svg`} />
+          </div>
+          <button onClick={retryHandler}>{strings.retry}</button>
+        </>
+      ) : (
+        <>
+          <div className="summary__process-label">
+            {label}
+            <img src={`/${status || "pending"}.svg`} />
+          </div>
+          <div className="summary__process-container">
+            <div className={step ? `${bar}-process` : bar}></div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
