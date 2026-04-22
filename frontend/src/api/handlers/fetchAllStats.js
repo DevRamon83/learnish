8bb9b7400d234418fd0d0d502448b123be54b7a3
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-22 17:44:42 +0200

    feat(front): add fetchAllStats

---
+++ frontend/src/api/handlers/fetchAllStats.js
@@ -0,0 +1,18 @@
+import { API_URLS } from "../../constants/apiUrls";
+import { fetchData } from "../fetchData";
+
+const fetchAllStats = async (signal) => {
+  const { base, dashboard } = API_URLS;
+  const apiUrl = base + dashboard.base + dashboard.allStats;
+
+  const method = "GET";
+  const credentials = "include";
+  const myData = null;
+  const response = await fetchData(apiUrl, method, credentials, myData, {
+    signal,
+  });
+
+  return response;
+};
+
+export default fetchAllStats;
