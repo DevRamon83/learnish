10003d3c9384590d74f63d0a6ed91251750a32bb
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-23 14:38:26 +0200

    feat(front): add SummaryErrorStats

--- frontend/src/ui/summary/ShowSummaryDetails.jsx
+++ frontend/src/ui/summary/ShowSummaryDetails.jsx
@@ -2,17 +2,24 @@ import { i18nAddresses } from "../../constants/i18nAddresses";
 import { classes } from "../../constants/components/dashboard";
 import { useLang } from "../../hooks/useLang";
 import SummaryScore from "./SummaryScore";
+import SummaryErrorStats from "./SummaryErrorStats";
 
 export default function ShowSummaryDetails({ data, stats }) {
   const { strings, lang } = useLang(i18nAddresses.summary);
 
   return (
-    <div className={classes.summary.detailsContainer}>
-      <SummaryScore strings={strings} stats={stats} data={data} />
-      <div className="summary__feedback">
-        <h4>{strings.score.rating}:</h4>
-        <p>{data.feedback}</p>
+    <>
+      <div className={classes.summary.scoreContainer}>
+        <SummaryScore strings={strings} stats={stats} data={data} />
+        <div className="summary__feedback">
+          <h4>{strings.score.rating}:</h4>
+          <p>{data.feedback}</p>
+        </div>
       </div>
-    </div>
+
+      <div className={classes.summary.errorsContainer}>
+        <SummaryErrorStats strings={strings} stats={stats} data={data} />
+      </div>
+    </>
   );
 }
