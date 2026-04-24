598e0a79b2ad15c9b7b9485918d9e182760d7f28
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-24 18:31:46 +0200

    feat(backend): add score to generateStats

--- backend/services/generateStats.js
+++ backend/services/generateStats.js
@@ -1,16 +1,18 @@
-import defineDate from "../helpers/stats/defineDate.js";
 import wordsCounter from "../helpers/stats/wordsCounter.js";
 import statsModel from "../models/stats.js";
+import summaryModel from "../models/summaries.js";
 import misalignmentHandler from "./misalignmentHandler.js";
+import bundle from "../../shared/index.js";
+const { defineDate } = bundle.helpers;
 
-const generateStats = async (ai, summary) => {
+const generateStats = async (summary) => {
   const mongoIDsummary = summary._id;
   try {
     const { day, month, year } = defineDate(summary.createdAt);
     const userId = summary.owner;
     const words = wordsCounter(summary.summary);
-    const mistakes = ai.mistakes.length;
-    const errorTypes = ai.errorCodes;
+    const mistakes = summary.mistakes.length;
+    const errorTypes = summary.errorCodes;
 
     await statsModel.findOneAndUpdate(
       { userId, year, month },
@@ -22,14 +24,29 @@ const generateStats = async (ai, summary) => {
             words,
             mistakes,
             errorTypes,
+            score: {
+              overall: summary.score.overall,
+              cohesion: summary.score.cohesion,
+              vocabulary: summary.score.vocabulary,
+              grammar: summary.score.grammar,
+            },
           },
         },
       },
       { upsert: true, new: true },
     );
+
+    const updateSummary = await summaryModel.findOneAndUpdate(
+      { _id: mongoIDsummary },
+      { misalignment: false },
+      { upsert: true, new: true },
+    );
+
+    return { error: false, summary: updateSummary };
   } catch (err) {
     console.error("stat error ", err);
     misalignmentHandler(mongoIDsummary);
+    return { error: true, errorMsg: err.message || err };
   }
 };
 
