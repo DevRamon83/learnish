b740affa34beef6145b6eb534f47a7de659e10d1
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-04-24 18:35:24 +0200

    fix(backend): add temporary patch to metadata in newSummary

--- backend/controllers/dashboard/newSummary.js
+++ backend/controllers/dashboard/newSummary.js
@@ -1,47 +1,45 @@
 import handleErrorResponse from "../../helpers/handleErrorResponse.js";
 import getVideoMetadata from "../../services/getVideoMetadata.js";
 import summaryModel from "../../models/summaries.js";
-import doCorrections from "../../services/doCorrections.js";
+
 const newSummary = async (req, res) => {
   const log = false;
-  let mySummary = null;
-  let userLang = null;
 
   try {
     const { id, shared } = req.context.auth;
 
     const { summary, idVideo, lang } = req.body;
-    userLang = lang;
+    const userLang = lang;
+    // const video = await getVideoMetadata(idVideo);
 
-    const video = await getVideoMetadata(idVideo);
+    const video = {
+      error: false,
+      title: "title patch",
+      channel: "channel patch",
+    };
 
     if (video.error) {
-      const errorMsg = video.errorMsg;
+      const errorMsg = "retryDraft";
       return handleErrorResponse(res, req, errorMsg, 409, log);
     }
 
     const data = {
       title: video.title,
       channel: video.channel,
-      thumbnail: video.thumbnail,
       videoID: idVideo,
       summary,
-      isDraft: true,
       shared,
       owner: id,
     };
 
-    mySummary = await summaryModel.create(data);
+    const mySummary = await summaryModel.create(data);
 
     // Respond immediately to the user to bypass AI analysis latency
-    res.status(200).json({ error: false, summaryId: mySummary._id });
+    res.status(200).json({ error: false, summary: mySummary });
   } catch (err) {
-    console.error("Error in login:", err);
+    console.error("Error in newSummary:", err);
     return handleErrorResponse(res, req, err.message, 500, log);
   }
-
-  // Process summary analysis asynchronously without affecting response time
-  doCorrections(mySummary, userLang);
 };
 
 export default newSummary;
