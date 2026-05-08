d378cf2ab4384b9787c5ab7a72ada8a753673f28
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-08 12:00:49 +0200

    refactor(backend): improve vocabularySchema

--- backend/models/vocabulary.js
+++ backend/models/vocabulary.js
@@ -2,46 +2,56 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const vocabularySchema = new Schema({
-  word: { type: String, required: true, unique: true },
-  index: { type: Number, required: true },
-  type: { type: String, required: true },
-  level: { type: String, required: true },
-  phonetics: {
-    us: { type: String, required: true },
-    uk: { type: String, required: true },
-  },
-  phrase: {
-    example: { type: String, default: false },
-    audioUs: { type: Boolean, default: false },
-    audioEn: { type: Boolean, default: false },
-  },
-  translations: {
-    word: {
+const vocabularySchema = new Schema(
+  {
+    word: { type: String, required: true },
+    phrase: { type: String },
+    index: { type: Number, required: true },
+    type: { type: String, required: true },
+    level: { type: String, required: true },
+    definition: { type: String },
+    flashcard: { type: Boolean, default: false },
+    metadata: {
+      storage: { type: String },
+      bucketImg: { type: String },
+      flashcardFile: { type: String },
+      word: {
+        bucket: { type: String },
+        path: { type: String },
+        us: { type: Boolean, required: true, default: false },
+        uk: { type: Boolean, required: true, default: false },
+        fileName: { type: String },
+      },
+      phrase: {
+        bucket: { type: String },
+        path: { type: String },
+        us: { type: Boolean, required: true, default: false },
+        uk: { type: Boolean, required: true, default: false },
+        fileName: { type: String },
+      },
+    },
+    phonetics: {
+      us: { type: String, required: true },
+      uk: { type: String, required: true },
+    },
+    translations: {
+      italian: { word: { type: String }, phrase: { type: String } },
+      spanish: { word: { type: String }, phrase: { type: String } },
+      french: { word: { type: String }, phrase: { type: String } },
+      portuguese: { word: { type: String }, phrase: { type: String } },
+    },
+    relations: {
       type: Map,
-      of: String,
+      of: [Schema.Types.ObjectId],
       default: {},
     },
-    phrase: {
-      type: Map,
-      of: String,
-      default: {},
-    },
   },
-  img: { type: Boolean, default: false },
-  pronunciation: {
-    audioUs: { type: Boolean, default: false },
-    audioEn: { type: Boolean, default: false },
-  },
-  definition: { type: String },
-  relations: {
-    type: Map,
-    of: [Schema.Types.ObjectId],
-    default: {},
-  },
-});
+  { timestamps: true },
+);
 
 vocabularySchema.index({ level: 1, type: 1 });
+vocabularySchema.index({ createdAt: -1 });
+vocabularySchema.index({ flashcard: -1, level: 1, type: 1 });
 
 const vocabularyModel = mongoose.model("Vocabulary", vocabularySchema);
 
