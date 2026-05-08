e1886d2655b895a7c7e50d2c9164483d96001df0
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-08 12:02:06 +0200

    feat(backend): add prompts for ai integration

---
+++ backend/ai/prompt/prompt.js
@@ -0,0 +1,18 @@
+export const mistralVocabularyPrompt = (myWord) => {
+  return `Starting from the word "${myWord}" provide: 
+  1) flashcard as boolean: if the word is suitable for generate a flashcard (meaning it must be possible to guess the word from the image), 
+  2) a string whit an example phrase (max 20 words)
+  3) for each one of this languages (italian, french, spanish, portuguese) a translation for both, word and example phrase
+  4) the definition of the initial word
+  `;
+};
+
+export const pollinationsImgPrompt = (myWord) => {
+  return encodeURIComponent(
+    `A cinematic still from a Studio Ghibli animation. The scene features ${myWord} as the main visual element. Hand-painted background, lush details, bright natural lighting. The image is a pure illustration with no typography.`,
+  );
+};
+
+export const pollinationsReadPrompt = (myWord, accent) => {
+  return encodeURIComponent(`${myWord}"`);
+};
