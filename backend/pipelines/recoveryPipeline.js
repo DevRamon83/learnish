971b7161e819fe71434d139c616f7dc84a5fead3
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-13 16:13:16 +0200

    feat(backend): add recoveryPipeline

---
+++ backend/pipelines/recoveryPipeline.js
@@ -0,0 +1,38 @@
+import pipelinesLogModel from "../models/logs/pipelinesLog.js";
+import vocabularyModel from "../models/vocabulary.js";
+import { attempt } from "./helpers/commons.js";
+import getWordData from "./helpers/wordData.js";
+
+const canDelete = async (id) => {
+  try {
+    const deletedDocument = await pipelinesLogModel.findByIdAndDelete(id);
+    console.log("deleted successfully ");
+  } catch (err) {
+    console.log("failed ", err);
+  }
+};
+
+const delay = async () => {
+  return new Promise((resolve) => setTimeout(resolve, 70000));
+};
+
+export const recoveryWordData = async () => {
+  const records = await pipelinesLogModel.find({});
+
+  for (let i = 0; i < records.length; i++) {
+    const wrongWord = records[i].word;
+    const wordObj = await vocabularyModel.findOne({ word: wrongWord });
+    console.log("myWord ", wrongWord);
+
+    const newWord = { ...wordObj };
+    const myWord = await attempt(getWordData, [wordObj]);
+
+    if (!myWord.error) {
+      await canDelete(records[i]._id);
+    } else {
+      console.log("failed");
+    }
+
+    await delay();
+  }
+};
