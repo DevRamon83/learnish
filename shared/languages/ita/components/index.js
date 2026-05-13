485d0876853e0c990de5c827d7b062e00280d0d3
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-13 10:06:47 +0200

    refactor(shared): reorganize ita language folder

--- shared/languages/ita/components/index.js
+++ shared/languages/ita/components/index.js
@@ -3,11 +3,7 @@ import summary from "./summary.json" with { type: "json" };
 import dashboard from "./dashboard.json" with { type: "json" };
 import layout from "./layout.json" with { type: "json" };
 import errors from "./errors.json" with { type: "json" };
-import faq from "./faq.json" with { type: "json" };
-import team from "./team.json" with { type: "json" };
 import metadata from "./metadata.json" with { type: "json" };
-import contact from "./contact.json" with { type: "json" };
-import price from "./price.json" with { type: "json" };
 
 const components = {
   auth,
@@ -15,11 +11,7 @@ const components = {
   dashboard,
   layout,
   errors,
-  faq,
-  team,
   metadata,
-  contact,
-  price,
 };
 
 export default components;
--- shared/languages/ita/pages/index.js
+++ shared/languages/ita/pages/index.js
@@ -1,7 +1,15 @@
 import homepage from "./homepage.json" with { type: "json" };
+import contact from "./contact.json" with { type: "json" };
+import price from "./price.json" with { type: "json" };
+import faq from "./faq.json" with { type: "json" };
+import team from "./team.json" with { type: "json" };
 
 const pages = {
   homepage,
+  faq,
+  team,
+  contact,
+  price,
 };
 
 export default pages;
