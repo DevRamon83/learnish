7b51c43773f1a93a819bbaee7ff697f11e5d094a
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-14 13:37:05 +0200

    feat(frontend): add title textInput to summary

--- frontend/src/forms/configs/summary.js
+++ frontend/src/forms/configs/summary.js
@@ -1,3 +1,11 @@
+const title = {
+  id: "title",
+  type: "text",
+  placeholder: null,
+  label: null,
+  required: true,
+};
+
 const youtube = {
   id: "youtube",
   type: "url",
@@ -20,11 +28,13 @@ const summary = {
 };
 
 const summaryConfigBuilder = (strings) => {
+  title.placeholder = strings.newSummaryForm.title.placeholder;
+  title.label = strings.newSummaryForm.title.label;
   youtube.placeholder = strings.newSummaryForm.youtube.placeholder;
   youtube.label = strings.newSummaryForm.youtube.label;
   summary.label = strings.newSummaryForm.summary.label;
   summary.counterLabel = strings.newSummaryForm.summary.counterLabel;
-  const array = [youtube, summary];
+  const array = [title, youtube, summary];
   return {
     configArray: array,
     isAsync: false,
