b8c21169cfb643ad76baa6924e78afccfffb7f70
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-18 19:33:40 +0200

    feat(frontend): add Lesson page

--- frontend/src/pages/Lesson.jsx
+++ frontend/src/pages/Lesson.jsx
@@ -3,18 +3,21 @@ import { useLang } from "../hooks/useLang";
 import { i18nAddresses } from "../constants/i18nAddresses";
 import { useState } from "react";
 import { useEffect } from "react";
+import Table from "../components/Table";
 
 export default function Lesson() {
   const { pathname } = useLocation();
-  const lessonID = pathname.replace("/user/lesson/", "");
+  const lessonID = pathname.replace("/user/lesson/", "").replaceAll("-", "_");
   const { strings } = useLang(i18nAddresses.englishLessons);
   const lessonObj = strings[lessonID];
   const markup = lessonObj.markup;
+  const table = lessonObj.table;
 
   return (
     <div className="lesson__main">
       <h1>{lessonObj.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: markup }} />
+      {table && <Table data={table} />}
     </div>
   );
 }
