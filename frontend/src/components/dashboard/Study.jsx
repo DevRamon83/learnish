75dbc60c8056a6b5a6ad803c51c2b5ee12b6c5fc
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-23 14:56:46 +0200

    feat(frontend): add Lesson tab to Study

--- frontend/src/components/dashboard/Study.jsx
+++ frontend/src/components/dashboard/Study.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import StudyMenu from "./StudyMenu";
 import Summaries from "./summaries/Summaries";
+import Lessons from "./Lessons";
 
 export default function Study() {
   const [activeTab, setActiveTab] = useState(0);
   return (
     <>
       <StudyMenu activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === 0 && <Lessons />}
       {activeTab === 1 && <Summaries />}
     </>
   );
