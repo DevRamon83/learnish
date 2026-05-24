68f1e1b9f7e77368f9fa1df9785a84bbedb6112f
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-24 18:55:18 +0200

    feat(frontend): add LessonMenu to Lesson page

--- frontend/src/pages/Lesson.jsx
+++ frontend/src/pages/Lesson.jsx
@@ -4,20 +4,36 @@ import { i18nAddresses } from "../constants/i18nAddresses";
 import { useState } from "react";
 import { useEffect } from "react";
 import Table from "../ui/Table";
+import LessonMenu from "../components/dashboard/LessonMenu";
+import { useUnlockedLessons } from "../hooks/useUnlockedLessons";
+import LessonLocked from "../ui/LessonLocked";
 
 export default function Lesson() {
   const { pathname } = useLocation();
   const lessonID = pathname.replace("/user/lesson/", "").replaceAll("-", "_");
   const { strings } = useLang(i18nAddresses.englishLessons);
-  const lessonObj = strings[lessonID];
+  const lessonObj = strings.get(lessonID);
   const markup = lessonObj.markup;
   const table = lessonObj.table;
 
+  const { userLessons, setUserLessons } = useUnlockedLessons();
+
   return (
-    <div className="lesson__main">
-      <h1>{lessonObj.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: markup }} />
-      {table && <Table data={table} />}
-    </div>
+    <>
+      {userLessons && (
+        <>
+          {userLessons.includes(lessonObj.index) ? (
+            <div className="lesson__main">
+              <h1>{lessonObj.title}</h1>
+              <div dangerouslySetInnerHTML={{ __html: markup }} />
+              {table && <Table data={table} />}
+              <LessonMenu strings={strings} lessonID={lessonID} />
+            </div>
+          ) : (
+            <LessonLocked />
+          )}
+        </>
+      )}
+    </>
   );
 }
