236a68c05539d1857e6068ce3a7fe3bb057875ea
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-25 19:40:10 +0200

    feat(backend): enforce daily limits based on user plan into newSummary

--- backend/controllers/dashboard/newSummary.js
+++ backend/controllers/dashboard/newSummary.js
@@ -1,11 +1,51 @@
 import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import statsModel from "../../models/stats.js";
 import summaryModel from "../../models/summaries.js";
+import { userModel } from "../../models/user.js";
+
+const todaySummaries = async (userId) => {
+  const todayDate = new Date();
+  const year = todayDate.getFullYear();
+  const month = todayDate.getMonth() + 1;
+  const today = todayDate.getDate();
+
+  const monthDoc = await statsModel.findOne({ userId, year, month }).lean();
+
+  if (!monthDoc || !monthDoc.dayStat) return 0;
+
+  const todayObj = monthDoc.dayStat.filter((stat) => stat.day === today);
+
+  return todayObj.length;
+};
+
+const availableSummary = (plan, summaries) => {
+  if (plan === "free") return false;
+  if (plan === "basic" && summaries >= 1) return false;
+  if (plan === "pro" && summaries >= 3) return false;
+
+  return true;
+};
 
 const newSummary = async (req, res) => {
   const log = false;
 
   try {
     const { id, shared } = req.context.auth;
+
+    const user = await userModel.findById(id).lean();
+
+    if (!user) {
+      const errorMsg = "user not found";
+      return handleErrorResponse(res, req, errorMsg, 404, true);
+    }
+
+    const createdToday = await todaySummaries(id);
+
+    const haveAvailableSummaries = availableSummary(user.plan, createdToday);
+
+    if (!haveAvailableSummaries) {
+      return res.status(400).json({ error: true, message: "unavailable" });
+    }
 
     const { summary, idVideo, lang, title } = req.body;
     const userLang = lang;
