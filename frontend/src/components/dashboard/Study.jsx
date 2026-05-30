d57d7b95dff567263687d9a595cfeb969c240df5
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-05-30 14:45:18 +0200

    feat(frontend): add Flashcards to Study

--- frontend/src/components/dashboard/Study.jsx
+++ frontend/src/components/dashboard/Study.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import StudyMenu from "./StudyMenu";
 import Summaries from "./summaries/Summaries";
 import Lessons from "./Lessons";
+import Flashcards from "./Flashcards";
 
 export default function Study() {
   const [activeTab, setActiveTab] = useState(0);
@@ -10,6 +11,7 @@ export default function Study() {
       <StudyMenu activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === 0 && <Lessons />}
       {activeTab === 1 && <Summaries />}
+      {activeTab === 2 && <Flashcards />}
     </>
   );
 }
