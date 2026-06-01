864ff9e312bfb27ed5982ee92e45b2d9c24e9294
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-01 10:42:59 +0200

    feat(backend): add checkPlan to flashcard

--- backend/controllers/dashboard/flashcards.js
+++ backend/controllers/dashboard/flashcards.js
@@ -1,12 +1,31 @@
 import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import { userModel } from "../../models/user.js";
 import vocabularyModel from "../../models/vocabulary.js";
 
-// need plan block logic (is a middleware)
+const checkPlan = (plan) => {
+  const planMap = {
+    free: ["A1", "A2"],
+    basic: ["A1", "A2", "B1", "B2"],
+    pro: ["A1", "A2", "B1", "B2", "C1", "C2"],
+  };
+
+  return planMap[plan];
+};
+
 const flashcards = async (req, res) => {
   const log = true;
   try {
-    const words = await vocabularyModel.find({ flashcard: true }).lean();
-    res.status(200).json({ error: false, words });
+    const userID = req.context.auth.id;
+    const userPlan = await userModel.findById(userID).select("plan").lean();
+    const filterBy = checkPlan(userPlan.plan);
+    const userWords = await vocabularyModel
+      .find({
+        flashcard: true,
+        level: { $in: filterBy },
+      })
+      .lean();
+
+    res.status(200).json({ error: false, userWords });
   } catch (err) {
     console.error("Error in flashcards:", err);
     return handleErrorResponse(res, req, err.message, 500, log);
