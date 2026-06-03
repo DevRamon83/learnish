54828018e07613eaef808c706dff031a2c652239
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-03 14:43:39 +0200

    refactor(frontend): add rows containers to FlashcardMenu

--- frontend/src/ui/FlashcardMenu.jsx
+++ frontend/src/ui/FlashcardMenu.jsx
@@ -13,28 +13,32 @@ export default function FlashcardMenu({
   const { setStart } = setters;
   return (
     <div className={classes.menu}>
-      <FlashcardHelpBtn
-        classes={classes}
-        setters={setters}
-        cards={cards}
-        states={states}
-      />
-      <ScoreAndPoints
-        strings={strings}
-        stringKey="score"
-        state={score}
-        scoreClass={classes.score}
-      />
+      <div className="flashcard__menu-topRow">
+        <FlashcardHelpBtn
+          classes={classes}
+          setters={setters}
+          cards={cards}
+          states={states}
+        />
+        <ScoreAndPoints
+          strings={strings}
+          stringKey="score"
+          state={score}
+          scoreClass={classes.score}
+        />
+      </div>
       <div className={classes.finish} onClick={() => setStart(false)}>
         <img src="/finish.svg" />
       </div>
-      <ScoreAndPoints
-        strings={strings}
-        stringKey="points"
-        state={points}
-        scoreClass={classes.points}
-      />
-      <FlashcardNextBtn setters={setters} states={states} classes={classes} />
+      <div className="flashcard__menu-bottomRow">
+        <ScoreAndPoints
+          strings={strings}
+          stringKey="points"
+          state={points}
+          scoreClass={classes.points}
+        />
+        <FlashcardNextBtn setters={setters} states={states} classes={classes} />
+      </div>
     </div>
   );
 }
