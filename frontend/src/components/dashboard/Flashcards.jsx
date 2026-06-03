4737084a8982b87a8661f337d32a3c47e60f4805
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-03 14:42:41 +0200

    feat(frontend): add intro to Flashcards

--- frontend/src/components/dashboard/Flashcards.jsx
+++ frontend/src/components/dashboard/Flashcards.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import fetchFlashcards from "../../api/handlers/fetchFlashcards";
 import FlashcardGame from "./FlashcardGame";
+import { useLang } from "../../hooks/useLang";
+import { i18nAddresses } from "../../constants/i18nAddresses";
 
 export default function Flashcards() {
   const [cards, setCards] = useState(null);
   const [start, setStart] = useState(false);
+  const { strings, lang } = useLang(i18nAddresses.flashcards);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -24,10 +27,26 @@ export default function Flashcards() {
   }, []);
 
   return (
-    <div>
-      <p>flashcards</p>
-      {cards && !start && <div onClick={() => setStart(true)}>start</div>}
-      {start && <FlashcardGame setStart={setStart} cards={cards} />}
+    <div className="flashcard__main">
+      {!start && (
+        <div className="flashcard__intro">
+          <p>{strings.intro}</p>
+          <h2>{strings.commands}</h2>
+          <ol className="flashcard__command">
+            <li>{strings.enter}</li>
+            <li>{strings.arrow}</li>
+            <li>{strings.esc}</li>
+          </ol>
+
+          <div
+            className="flashcard__start"
+            onClick={() => (cards ? setStart(true) : null)}
+          >
+            {cards ? strings.start : strings.wait}
+          </div>
+        </div>
+      )}
+      {start && cards && <FlashcardGame setStart={setStart} cards={cards} />}
     </div>
   );
 }
