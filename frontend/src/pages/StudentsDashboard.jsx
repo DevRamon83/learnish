493862ae4fa03619393631f75adf1dec5a40a37e
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-07 13:51:24 +0200

    refactor(frontend): import dashboardStudentsTabs into StudentDashboard

--- frontend/src/pages/StudentsDashboard.jsx
+++ frontend/src/pages/StudentsDashboard.jsx
@@ -4,16 +4,21 @@ import Study from "../components/dashboard/Study";
 import useRetriveStats from "../hooks/useRetriveStats";
 import { classes } from "../constants/components/dashboard";
 import useLogout from "../hooks/useLogout";
+import bundle from "shared";
+const { dashboardStudentsTabs } = bundle.constants;
 
 export default function StudentsDashboard() {
   const [currentTab, setCurrentTab] = useState("study");
   useRetriveStats();
-
   useLogout(currentTab);
 
   return (
     <main className={classes.main}>
-      <DashboardMenu setCurrentTab={setCurrentTab} currentTab={currentTab} />
+      <DashboardMenu
+        tabs={dashboardStudentsTabs}
+        setCurrentTab={setCurrentTab}
+        currentTab={currentTab}
+      />
       <div className={classes.core}>{currentTab === "study" && <Study />}</div>
     </main>
   );
