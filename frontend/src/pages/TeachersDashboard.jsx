02bbf95fb9df740f02880b59bdf225f7f8add007
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-07 13:52:07 +0200

    feat(frontend): add TeachersDashboard

--- frontend/src/pages/TeachersDashboard.jsx
+++ frontend/src/pages/TeachersDashboard.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import bundle from "shared";
+import useLogout from "../hooks/useLogout";
+import DashboardMenu from "../components/dashboard/DashboardMenu";
+import { classes } from "../constants/components/dashboard";
+import YourStudents from "../components/dashboard/YourStudents";
+const { dashboardTeachersTabs } = bundle.constants;
+
 export default function TeachersDashboard() {
+  const [currentTab, setCurrentTab] = useState("students");
+  useLogout(currentTab);
+
   return (
     <>
-      <p>ciao</p>
-      <p>ciao</p>
+      <main className={classes.main}>
+        <DashboardMenu
+          tabs={dashboardTeachersTabs}
+          setCurrentTab={setCurrentTab}
+          currentTab={currentTab}
+        />
+        <div className={classes.core}>
+          {currentTab === "students" && <YourStudents />}
+        </div>
+      </main>
     </>
   );
 }
