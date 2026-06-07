1e8db4dd6ce8d97eeca7f8f597bc06a2e5cbc261
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-07 13:50:29 +0200

    refactor(frontend): map tabs into DashboardMenu

--- frontend/src/components/dashboard/DashboardMenu.jsx
+++ frontend/src/components/dashboard/DashboardMenu.jsx
@@ -1,35 +1,19 @@
 import { classes } from "../../constants/components/dashboard";
 import IconBtn from "../../ui/buttons/IconBtn";
 
-export default function DashboardMenu({ setCurrentTab, currentTab }) {
+export default function DashboardMenu({ tabs, setCurrentTab, currentTab }) {
   const { dashBtn, dashActiveBtn, menu } = classes;
-
   return (
     <div className={menu}>
-      <IconBtn
-        iconClass={currentTab === "study" ? dashActiveBtn : dashBtn}
-        img={"/study.svg"}
-        handler={setCurrentTab}
-        value={"study"}
-      />
-      <IconBtn
-        iconClass={currentTab === "stats" ? dashActiveBtn : dashBtn}
-        img={"/stats.svg"}
-        handler={setCurrentTab}
-        value={"stats"}
-      />
-      <IconBtn
-        iconClass={currentTab === "settings" ? dashActiveBtn : dashBtn}
-        img={"/settings.svg"}
-        handler={setCurrentTab}
-        value={"settings"}
-      />
-      <IconBtn
-        iconClass={currentTab === "logout" ? dashActiveBtn : dashBtn}
-        img={"/logout.svg"}
-        handler={setCurrentTab}
-        value={"logout"}
-      />
+      {tabs.map((tab) => (
+        <IconBtn
+          key={`tab${tab}`}
+          iconClass={currentTab === tab ? dashActiveBtn : dashBtn}
+          img={`/${tab}.svg`}
+          handler={setCurrentTab}
+          value={tab}
+        />
+      ))}
     </div>
   );
 }
