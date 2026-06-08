2310749eed2bc353829687de8e772be0bd6b53b7
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-08 18:28:48 +0200

    feat(backend): add saveProfilePic

---
+++ backend/controllers/update/saveProfilePic.js
@@ -0,0 +1,45 @@
+import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import uploadFile from "../../helpers/uploadFile.js";
+import { userModel } from "../../models/user.js";
+
+const saveProfilePic = async (req, res) => {
+  const log = false;
+
+  try {
+    const fileData = req.file;
+
+    const data = {
+      buffer: fileData.buffer,
+      fileName: fileData.name,
+      folder: "/profilePics",
+      metaData: {
+        bucket: "users",
+        type: fileData.mimetype,
+      },
+    };
+
+    const upload = await uploadFile(data);
+
+    if (upload.error) {
+      return handleErrorResponse(res, req, upload.error, 500, log);
+    }
+
+    await userModel.findOneAndUpdate(
+      { _id: req.context.auth.id },
+      {
+        $set: {
+          "profilePic.storage": "supabase",
+          "profilePic.bucketImg": "users",
+          "profilePic.fileName": fileData.name,
+        },
+      },
+    );
+
+    return res.status(200).json({ error: false, picName: fileData.name });
+  } catch (err) {
+    console.error("Error in saveProfilePic:", err);
+    return handleErrorResponse(res, req, err.message, 500, log);
+  }
+};
+
+export default saveProfilePic;
