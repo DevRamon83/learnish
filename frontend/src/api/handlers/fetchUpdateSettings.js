668d056229bc8e5d60f3e11a1b937163b15f5fec
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-09 17:52:12 +0200

    feat(frontend): add fetchUpdateSettings

---
+++ frontend/src/api/handlers/fetchUpdateSettings.js
@@ -0,0 +1,15 @@
+import { API_URLS } from "../../constants/apiUrls";
+import { fetchData } from "../fetchData";
+
+const fetchUpdateSettings = async (data) => {
+  const { base, update } = API_URLS;
+  const apiUrl = base + update.base + update.settings;
+
+  const method = "POST";
+  const credentials = "include";
+  const response = await fetchData(apiUrl, method, credentials, data);
+
+  return response;
+};
+
+export default fetchUpdateSettings;
