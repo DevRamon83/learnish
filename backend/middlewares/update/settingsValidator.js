414deee0feeb86244931c4cb711d64dbb904c850
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-09 17:43:52 +0200

    feat(backend): add settingsValidator

---
+++ backend/middlewares/update/settingsValidator.js
@@ -0,0 +1,36 @@
+import bundle from "shared";
+import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+const { plans } = bundle.constants;
+
+const dispatchValidator = (fieldName, data) => {
+  switch (fieldName) {
+    case "plan":
+      return plans.includes(data[fieldName]);
+
+    default:
+      return false;
+  }
+};
+
+const stopIt = (req, res, fieldName = "payload structure") => {
+  const log = true;
+  const errorMessage = `invalid settingsValidator ${fieldName}`;
+  console.error(errorMessage);
+  handleErrorResponse(res, req, errorMessage, 400, log);
+};
+
+const settingsValidator = async (req, res, next) => {
+  const data = req.body;
+
+  const fieldArray = Object.keys(data);
+  const fieldName = fieldArray[0];
+
+  if (fieldArray.length !== 1) return stopIt(req, res, fieldName);
+  const isValid = dispatchValidator(fieldName, data);
+
+  if (!isValid) return stopIt(req, res, fieldName);
+
+  next();
+};
+
+export default settingsValidator;
