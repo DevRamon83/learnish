f119c9922b77e2588a8825f76bcd3f43c91ee9a5
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-09 17:40:51 +0200

    feat(backend): add profilePic to the check response

--- backend/controllers/auth/check.js
+++ backend/controllers/auth/check.js
@@ -1,6 +1,7 @@
 import createTokenConfigs from "../../helpers/token/createTokenConfigs.js";
 import setTokenAndCookie from "../../helpers/token/setTokenAndCookie.js";
 import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import { userModel } from "../../models/user.js";
 
 const check = async (req, res) => {
   try {
@@ -12,12 +13,15 @@ const check = async (req, res) => {
       await setTokenAndCookie(res, refreshConfig);
     }
 
+    const user = await userModel.findById(payload.id).lean();
+
     const response = {
       error: false,
       username: payload.username,
       id: payload.id,
       plan: payload.plan,
       type: payload.type,
+      pic: user.profilePic,
     };
 
     return res.status(200).json(response);
