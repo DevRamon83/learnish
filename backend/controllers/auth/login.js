134d46c75666cae29868d97734597282bb68d02f
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-09 17:42:50 +0200

    feat(backend): add profilePic to the configData login

--- backend/controllers/auth/login.js
+++ backend/controllers/auth/login.js
@@ -35,7 +35,9 @@ const login = async (req, res) => {
       id: user._id,
       plan: user.plan,
       type: user.userType,
+      pic: user.profilePic,
     };
+
     const { accessConfig, refreshConfig } = createTokenConfigs(configData);
 
     await setTokenAndCookie(res, accessConfig);
