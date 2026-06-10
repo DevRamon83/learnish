4d7f3f70485d5a2ce2cf9746f8aa01206aa0e2d8
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-10 11:32:21 +0200

    fix(frontend): rename dataSignup to data in fetchLogin

--- frontend/src/api/handlers/fetchLogin.js
+++ frontend/src/api/handlers/fetchLogin.js
@@ -1,13 +1,13 @@
 import { API_URLS } from "../../constants/apiUrls";
 import { fetchData } from "../fetchData";
 
-const fetchLogin = async (dataSignup) => {
+const fetchLogin = async (data) => {
   const { base, auth } = API_URLS;
   const apiUrl = base + auth.base + auth.login;
 
   const method = "POST";
   const credentials = "include";
-  const response = await fetchData(apiUrl, method, credentials, dataSignup);
+  const response = await fetchData(apiUrl, method, credentials, data);
 
   return response;
 };
