af6cd4747450747e6ff9528ff0cd8bd7fb5cccab
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-10 16:41:07 +0200

    feat(backend): add retrieve

---
+++ backend/controllers/auth/retrieve.js
@@ -0,0 +1,23 @@
+import handleErrorResponse from "../../helpers/handleErrorResponse.js";
+import { userModel } from "../../models/user.js";
+
+const retrieve = async (req, res) => {
+  const log = true;
+  const ban = true;
+  try {
+    const data = req.body;
+    const userID = req.context.auth.id;
+
+    const datum = await userModel.findById(userID).select(data.retrieve);
+    if (!datum) {
+      return handleErrorResponse(res, req, "invalid retrieve", 400, log, ban);
+    }
+
+    return res.status(201).json(datum);
+  } catch (err) {
+    console.error("Error in retrieve:", err);
+    return handleErrorResponse(res, req, err.message, 500, log);
+  }
+};
+
+export default retrieve;
