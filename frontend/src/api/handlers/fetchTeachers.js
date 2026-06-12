124dc4872fc26cd72f9c3330e34035ae5733052c
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-12 16:22:27 +0200

    feat(frontend): add fetchTeachers

---
+++ frontend/src/api/handlers/fetchTeachers.js
@@ -0,0 +1,18 @@
+import { API_URLS } from "../../constants/apiUrls";
+import { fetchData } from "../fetchData";
+
+const fetchTeachers = async (signal) => {
+  const { base, dashboard } = API_URLS;
+  const apiUrl = base + dashboard.base + dashboard.teachers;
+
+  const method = "GET";
+  const credentials = "include";
+  const myData = null;
+  const response = await fetchData(apiUrl, method, credentials, myData, {
+    signal,
+  });
+
+  return response;
+};
+
+export default fetchTeachers;
