f0af5ef0fe74d267d5a6159dcfaef0aaa93079e1
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-13 15:12:41 +0200

    feat(frontend): add teacher handling (Settings)

--- frontend/src/components/dashboard/Settings.jsx
+++ frontend/src/components/dashboard/Settings.jsx
@@ -2,15 +2,31 @@ import { useSelector } from "react-redux";
 import { useAuth } from "../../hooks/useAuth";
 import Pic from "./settings/Pic";
 import Plan from "./settings/Plan";
+import Email from "./settings/Email";
+import Password from "./settings/Password";
+import MyTeacher from "./settings/MyTeacher";
+import { useLang } from "../../hooks/useLang";
+import { i18nAddresses } from "../../constants/i18nAddresses";
+import TeacherContract from "./settings/TeacherContract";
+import Currency from "./settings/Currency";
 
-export default function Settings() {
+export default function Settings({ userType }) {
   const user = useSelector((state) => state.auth.user);
-  const planConfig = { type: "text", id: "plan" };
+  const { lang, strings } = useLang(i18nAddresses.settings);
 
   return (
     <div className="settings__main">
-      <Pic user={user} />
-      <Plan user={user} config={planConfig} />
+      <Pic strings={strings} user={user} />
+      <Plan strings={strings} user={user} />
+      <Email strings={strings} />
+      <Password strings={strings} />
+      {userType === "student" && <MyTeacher strings={strings} />}
+      {userType === "teacher" && (
+        <>
+          <Currency strings={strings} user={user} />
+          <TeacherContract strings={strings} lang={lang} />
+        </>
+      )}
     </div>
   );
 }
