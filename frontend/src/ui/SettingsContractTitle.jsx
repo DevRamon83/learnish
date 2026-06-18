65f15945e691130801cfdef1007299b9eb51c8ec
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-18 14:20:23 +0200

    feat(frontend): add SettingsContractTitle

---
+++ frontend/src/ui/SettingsContractTitle.jsx
@@ -0,0 +1,38 @@
+import ActivateService from "../components/dashboard/settings/ActivateService";
+import Deactivate from "../components/dashboard/settings/Deactivate";
+
+export default function SettingsContractTitle({
+  strings,
+  setExist,
+  exist,
+  setUserField,
+  classes,
+  contract,
+  lang,
+}) {
+  return (
+    <div className={classes.titleContainer}>
+      <h3>{strings.contract[contract]}</h3>
+      {contract !== "subscription" && exist && (
+        <Deactivate
+          strings={strings}
+          setExist={setExist}
+          setUserField={setUserField}
+          contract={contract}
+          classes={classes}
+        />
+      )}
+
+      {contract !== "subscription" && !exist && (
+        <ActivateService
+          service={contract}
+          strings={strings}
+          lang={lang}
+          setUserField={setUserField}
+          setExist={setExist}
+          classes={classes}
+        />
+      )}
+    </div>
+  );
+}
