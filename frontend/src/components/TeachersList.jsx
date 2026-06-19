c631401267f617edd8851badf6eb3f6fa96b9340
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-19 19:07:56 +0200

    feat(frontend): add ChangeTeacherMenu to TeachersList

--- frontend/src/components/TeachersList.jsx
+++ frontend/src/components/TeachersList.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import fetchTeachers from "../api/handlers/fetchTeachers";
-import { useHideOverflow } from "../hooks/useHideOverflow";
 import useScrollToTop from "../hooks/useScrollToTop";
-import getPicUrl from "../helpers/getPicUrl";
+import TeachersProfile from "../ui/teachers/TeachersProfile";
+import ChangeTeacherMenu from "../ui/teachers/ChangeTeacherMenu";
 
-export default function TeachersList({ setChangeTeacher }) {
+export default function TeachersList({ setChangeTeacher, strings, classes }) {
   const [teachers, setTeachers] = useState([]);
-  useHideOverflow(true);
+  const [currentTeacher, setCurrentTeacher] = useState(0);
+  const [next, setNext] = useState("");
+  const [previous, setPrevious] = useState("");
   useScrollToTop();
 
   useEffect(() => {
@@ -14,7 +16,6 @@ export default function TeachersList({ setChangeTeacher }) {
 
     const retrive = async () => {
       const res = await fetchTeachers(controller.signal);
-      console.log("res ", res);
       if (res.error) {
         // error handler
       } else {
@@ -27,17 +28,47 @@ export default function TeachersList({ setChangeTeacher }) {
     return () => controller.abort();
   }, []);
 
-  console.log(teachers);
+  useEffect(() => {
+    if (currentTeacher === 0) {
+      setPrevious(strings.start);
+    } else {
+      setPrevious(strings.back);
+    }
+
+    if (currentTeacher === teachers.length - 1) {
+      setNext(strings.end);
+    } else {
+      setNext(strings.next);
+    }
+  }, [currentTeacher]);
+
+  const navigationHandler = (action) => {
+    const step = action === "next" ? 1 : -1;
+    const end = action === "next" ? teachers.length - 1 : 0;
+
+    if (currentTeacher !== end) {
+      setCurrentTeacher((prev) => prev + step);
+    }
+  };
+
   return (
-    <div className="settings__teachersList">
-      <p>list</p>
-      {teachers.map((teacher) => (
-        <div key={teacher._id}>
-          <img src={getPicUrl(teacher)} />
-          <div>{teacher.username}</div>
-        </div>
-      ))}
-      <div onClick={() => setChangeTeacher(false)}>X</div>
+    <div className={classes.teacherList}>
+      <h1>{strings.list}</h1>
+      {teachers.length !== 0 && (
+        <TeachersProfile
+          teacher={teachers[currentTeacher]}
+          strings={strings}
+          classes={classes}
+        />
+      )}
+      <ChangeTeacherMenu
+        classes={classes}
+        navigationHandler={navigationHandler}
+        setChangeTeacher={setChangeTeacher}
+        strings={strings}
+        previous={previous}
+        next={next}
+      />
     </div>
   );
 }
