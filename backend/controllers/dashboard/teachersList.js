40625f33fe5892b47b5b6ce871e58c05f92081d4
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-19 19:15:41 +0200

    fix(backend): optimize query to teacherList

--- backend/controllers/dashboard/teachersList.js
+++ backend/controllers/dashboard/teachersList.js
@@ -3,8 +3,8 @@ import { Teacher } from "../../models/user.js";
 const teachersList = async (req, res) => {
   const log = true;
   try {
-    const teachers = await Teacher.find({})
-      .select("username taughtLang contacts profilePic")
+    const teachers = await Teacher.find({ "contract.isComplete": true })
+      .select("username taughtLang contract profilePic currency")
       .sort({ createdAt: -1 })
       .lean();
 
