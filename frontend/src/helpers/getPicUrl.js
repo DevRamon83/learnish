7bfb4046d4fc42c6bcac1d898cbe0e3d8a4df56f
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-06-19 19:10:45 +0200

    fix(frontend): add fallback to getPicUrl

--- frontend/src/helpers/getPicUrl.js
+++ frontend/src/helpers/getPicUrl.js
@@ -1,14 +1,15 @@
 import { userBaseURL } from "../constants/components/dashboard";
 
 const getPicUrl = (user) => {
-  if (!user.pic || Object.keys(user.pic).length === 0) {
+  const picObj = user.pic || user.profilePic;
+  if (!picObj || Object.keys(picObj).length === 0) {
     return `/noPic.svg`;
   }
 
   const now = Date.now();
 
-  if (user.pic.storage === "supabase") {
-    return `${userBaseURL}${user.pic.bucketImg}/profilePics/${user.pic.fileName}?t=${now}`;
+  if (picObj.storage === "supabase") {
+    return `${userBaseURL}${picObj.bucketImg}/profilePics/${picObj.fileName}?t=${now}`;
   }
 };
 
