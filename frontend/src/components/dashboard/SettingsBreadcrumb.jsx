545f3b11af3851128e274328e0ba9fc4e06216b6
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-07-01 13:50:55 +0200

    refactor(frontend): simplify SettingsBreadcrumb

--- frontend/src/components/dashboard/SettingsBreadcrumb.jsx
+++ frontend/src/components/dashboard/SettingsBreadcrumb.jsx
@@ -2,40 +2,25 @@ export default function SettingsBreadcrumb({ props }) {
   const { userCards, strings, setCard, toggle, card } = props;
 
   return (
-    <>
-      <div className="settings__breadcrumbContainer-desk">
-        {!toggle &&
-          userCards.map((value) => (
-            <div
-              className={
-                card === value
-                  ? "settings__breadcrumb-activeBtn"
-                  : "settings__breadcrumb-btn"
-              }
-              onClick={() => setCard(value)}
-              key={`breadcrumb_${value}`}
-            >
+    <div className="settings__breadcrumbContainer">
+      {!toggle &&
+        userCards.map((value) => (
+          <div
+            className={
+              card === value
+                ? "settings__breadcrumb-activeBtn"
+                : "settings__breadcrumb-btn"
+            }
+            onClick={() => setCard(value)}
+            key={`breadcrumb_${value}`}
+          >
+            <img src={`/${value.toLowerCase()}.svg`} />
+
+            <div className="settings__breadcrumb-text">
               {strings.settingsCard[value]}
             </div>
-          ))}
-      </div>
-
-      <div className="settings__breadcrumbContainer-mobile">
-        {!toggle &&
-          userCards.map((value) => (
-            <div
-              className={
-                card === value
-                  ? "settings__breadcrumb-activeBtnMobile"
-                  : "settings__breadcrumb-btnMobile"
-              }
-              onClick={() => setCard(value)}
-              key={`breadcrumb_${value}`}
-            >
-              <img src={`/${value.toLowerCase()}.svg`} />
-            </div>
-          ))}
-      </div>
-    </>
+          </div>
+        ))}
+    </div>
   );
 }
