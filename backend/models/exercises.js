34f84419be631cbfb0a7d5d1d71211440ed34cf4
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-07-09 18:01:11 +0200

    feat(backend): add exercises model

---
+++ backend/models/exercises.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  index: { type: String },
+  targetLang: {
+    type: String,
+    enum: ["english"],
+  },
+  type: {
+    type: String,
+    enum: ["multipleChoice"],
+  },
+  targetLang: { type: String },
+  instructions: { type: String },
+  question: {
+    italian: { type: String },
+    spanish: { type: String },
+    french: { type: String },
+    portuguese: { type: String },
+    english: { type: String },
+  },
+  options: {
+    italian: { type: [String] },
+    spanish: { type: [String] },
+    french: { type: [String] },
+    portuguese: { type: [String] },
+    english: { type: [String] },
+  },
+  answer: {
+    italian: { type: [String] },
+    spanish: { type: [String] },
+    french: { type: [String] },
+    portuguese: { type: [String] },
+    english: { type: [String] },
+  },
+});
+
+const exerciseModel = mongoose.model("Exercise", ExerciseSchema);
+
+export default exerciseModel;
