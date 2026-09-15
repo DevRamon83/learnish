30d0b41870fb9bc430078ad42db7f31c198362b0
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-15 11:22:44 +0200

    refactor(frontend): remove obsolete imports

--- frontend/src/components/dashboard/summaries/Summaries.jsx
+++ frontend/src/components/dashboard/summaries/Summaries.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import fetchSummaries from "../../../api/handlers/fetchSummaries";
 import SummaryCard from "./SummaryCard";
 import FetchObserver from "../../../api/FetchObserver";
