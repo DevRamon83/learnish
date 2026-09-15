5bf0562f861bb3273e4f7f457a7e345ebfd45ced
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-15 11:21:24 +0200

    refactor(frontend): add useLang to FetchObserver

--- frontend/src/api/FetchObserver.jsx
+++ frontend/src/api/FetchObserver.jsx
@@ -1,11 +1,22 @@
+import { i18nAddresses } from "../constants/i18nAddresses";
+import { useLang } from "../hooks/useLang";
+
 export default function FetchObserver({ fetchStatus, caller, setRetry }) {
+  const { strings } = useLang(i18nAddresses.errors);
+
   return (
     <>
-      {fetchStatus === "void" && <div>Nessun {caller} presente</div>}
+      {fetchStatus === "void" && (
+        <div>
+          {strings.nothing} {caller} {strings.present}
+        </div>
+      )}
       {fetchStatus === "fail" && (
         <div>
-          <p>Errore di connessione</p>
-          <button onClick={() => setRetry((prev) => prev + 1)}>Riprova</button>
+          <p>{strings.connection}</p>
+          <button onClick={() => setRetry((prev) => prev + 1)}>
+            {strings.tryAgain}
+          </button>
         </div>
       )}
     </>
