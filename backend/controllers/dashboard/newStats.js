af2ff03ee4d5fe24f1a4be3d0e46b1501f75b851
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-09-15 16:31:42 +0200

    fix(backend): add summaryFinder to newStats

--- backend/controllers/dashboard/newStats.js
+++ backend/controllers/dashboard/newStats.js
@@ -1,20 +1,35 @@
 import handleErrorResponse from "../../helpers/handleErrorResponse.js";
 import generateStats from "../../services/generateStats.js";
+import summaryModel from "../../models/summaries.js";
+
+const summaryFinder = async (id, summary) => {
+  if (!id && !summary) return false;
+  if (summary) return summary;
+  return await summaryModel.findById(id);
+};
 
 const newStats = async (req, res) => {
   const log = true;
 
   try {
-    const { summary } = req.body;
+    const { id, summary } = req.body;
     const user = req.context.auth;
-    const owner = summary.owner;
 
-    if (user.id !== owner) {
+    const mySummary = await summaryFinder(id, summary);
+
+    if (!mySummary) {
+      const errorMsg = "notFound";
+      return handleErrorResponse(res, req, errorMsg, 404, log);
+    }
+
+    const owner = mySummary.owner;
+
+    if (user.id !== owner.toString()) {
       const errorMsg = "Unauthorized";
       return handleErrorResponse(res, req, errorMsg, 409, log);
     }
 
-    const stats = await generateStats(summary);
+    const stats = await generateStats(mySummary);
 
     if (stats.error) {
       const errorMsg = stats.errorMsg;
